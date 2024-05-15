460ba484b5adb777d7d3dc4e859d01504a65fc6f
Jiyakhan321 <168368019+Jiyakhan321@users.noreply.github.com>
2024-05-15 15:37:21 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+//Answer:1
+let age = +prompt("Enter your age");
+if (age > 18) {
+    console.log("You are eligible for vote");
+}
+else{
+    console.log("You are not eligible for vote");
+}
+
+//Answer:2
+let temprature = +prompt("check if the temprature");
+if (temprature > 25) {
+    console.log("It's a hot day");
+}
+else{
+    console.log("It's a normal day");
+}
+
+//Answer:3
+let studen_grade = +prompt("Enter your score");
+if(studen_grade >= 60) {
+    console.log("You passed the test");
+}
+else{
+    console.log("You failed the test");
+}
+
+//Answer:4
+let raining = true;
+if (raining ) {console.log("'Remember to bring an umbrella.");}
+else{
+console.log("enjoy the sunny weather!");}
+
+//Answer:5
+let type = +prompt("Enter your age");
+if (age > 18) {
+    console.log("You are an adult");
+}
+else if(age < 18){
+    console.log("You are a minor");
+}
+
+//Answer:5
+let number = +prompt("Enter the number")
+if (number > 0) {
+    console.log("The number is [positive");
+}
+else if (number < 0) {
+    console.log("The number is negative");
+}
+else if("number == 0") {
+    console.log("The number is zero");
+}
+else{
+    console.log("all number are integer");
+}
